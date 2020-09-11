84bfb31df393557fc12fc885016589cad3d81769
username <user@email.mail>
2020-09-11 23:39:48 +0900

    add clear command

--- index.js
+++ index.js
@@ -179,6 +179,10 @@ client.on('message', (message) => {
     return message.channel.send("ㄹㅇ ㅋㅋ 만 쳐라");
   }
 
+  if(message.content == "주혁아"){
+    return message.channel.send("곧 가니까 말걸지 마라 야발련아");
+  }
+
   if(message.content == '!서버상태') {
     let embed = new Discord.RichEmbed()
     let img = 'https://ww.namu.la/s/2afc61b884339ea4981262dc5c2bc667e2bbf58ab51aa2ddfb5ffedd43b1b1c1b1a7833ee9d5c5fd6c55a48feb82d6939abb353291f76006d7e65e0a35a78a6de3f0a0a0301b247cb4fe0acf900ff122f60dbe1eb4ec95ea90d791ef2f208df37f5779cf3ce0caeb07c5ab7b59df33a4';
